29455870a3fa46e73f353b547d5d8c353b32cf23
kishor <kishorebsc12@gmail.com>
2025-08-30 11:25:23 +0530

    added

--- catalogue.tf
+++ catalogue.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "catalogue" {
-    ami = var.ami_id
+  ami = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
   tags = {
