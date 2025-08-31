d17a8b9bfd1a7fec679118a54f1f16f77e315f8b
kishor <kishorebsc12@gmail.com>
2025-08-31 16:06:15 +0530

    added

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -42,7 +42,7 @@ resource "null_resource" "name" {
     }
 
     inline = [ 
-      "sudo pip3.11 install ansible",
+      "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/Thippareddykishor/roboshop-ansible.git roboshop.yml -e user=ec2-user -e password=DevOps321 -e env=${var.env} -e component_name=${var.name}"
      ]
   }
