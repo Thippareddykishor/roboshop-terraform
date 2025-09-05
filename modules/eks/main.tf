2a5de159b24a33d5478284067b8e59dc88010858
kishor <kishorebsc12@gmail.com>
2025-09-05 13:00:34 +0530

    added

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -22,7 +22,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn = aws_iam_role.node-role.arn
   subnet_ids = var.subnets
   instance_types = each.value["instance_types"]
-  capacity_type = ["SPOT"]
+  capacity_type = "SPOT"
 
   scaling_config {
     desired_size = each.value["min_nodes"]
