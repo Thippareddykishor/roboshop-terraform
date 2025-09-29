0236a798ea012547f366c275bba5cd3dec37c68f
kishor <kishorebsc12@gmail.com>
2025-09-29 08:20:42 +0530

    added

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,6 +1,6 @@
 
-resource "aws_eks_addon" "addons" {
-  for_each = var.addons
-  cluster_name = aws_eks_cluster.main.name
-  addon_name = each.key
-}
+# resource "aws_eks_addon" "addons" {
+#   for_each = var.addons
+#   cluster_name = aws_eks_cluster.main.name
+#   addon_name = each.key
+# }
