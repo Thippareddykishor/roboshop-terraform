d6578fe214899d8bca44e30802a2375e811643d5
kishor <kishorebsc12@gmail.com>
2025-09-29 12:24:13 +0530

    added

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -4,3 +4,15 @@
 #   cluster_name = aws_eks_cluster.main.name
 #   addon_name = each.key
 # }
+
+resource "null_resource" "argocd" {
+  depends_on = [ null_resource.kubeconfig ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+    kubectl create ns argocd
+    kubectl apply -n argocd  -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml 
+    kubectl patch svc argocd-server -n argocd --patch '{"spec": {"type" : "Loadbalancer"}}'
+    EOF
+  }
+}
