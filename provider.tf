de8831e915bc19c87bde41d5eb0847ebbe1ff248
kishor <kishorebsc12@gmail.com>
2025-09-30 18:07:25 +0530

    added

--- provider.tf
+++ provider.tf
@@ -19,14 +19,14 @@ provider "vault" {
 #   config_path = "~/.kube/config"
 # }
 
-# terraform {
-#   required_providers {
-#     helm = {
-#       source  = "hashicorp/helm"
-#       version = ">= 2.4.1" # or latest
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1" # or latest
+    }
+  }
+}
 
 
 provider "helm" {
