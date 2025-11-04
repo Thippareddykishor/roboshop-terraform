b9f5931ae95b884e39aeb9dbb9948cdc7726257c
kishor <kishorebsc12@gmail.com>
2025-11-04 19:32:10 +0530

    added

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -225,16 +225,16 @@ EOF
 
 # config reload
 
-# resource "helm_release" "wave-config-reloader" {
-#   depends_on = [ null_resource.kubeconfig ]
-#   name = "wave"
-#   repository = "https://wave-k8s.github.io/wave/"
-#   chart = "wave"
-#   namespace = "kube-system"
-#   wait = "false"
+resource "helm_release" "wave-config-reloader" {
+  depends_on = [ null_resource.kubeconfig ]
+  name = "wave"
+  repository = "https://wave-k8s.github.io/wave/"
+  chart = "wave"
+  namespace = "kube-system"
+  wait = "false"
 
-#   set = [{
-#     name = "webhooks.enabled"
-#     value = true
-#   }]
-# }
+  set = [{
+    name = "webhooks.enabled"
+    value = true
+  }]
+}
