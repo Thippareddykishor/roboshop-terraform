37e74dc58ea25f354411211f438c1f7ad746c279
kishor <kishorebsc12@gmail.com>
2025-11-11 17:44:32 +0530

    added

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
-module "ec2" {
-  for_each = var.db_instances
-  source                  = "./modules/ec2"
-  instance_type           = each.value["instance_type"]
-  name                    = each.key
-  ami_id                  = each.value["ami_id"]
-  vpc_security_group_ids  = var.vpc_security_group_ids
-  zone_id                 = var.zone_id
-  env                     = var.env
-  vault_token             = var.vault_token
-}
+# module "ec2" {
+#   for_each = var.db_instances
+#   source                  = "./modules/ec2"
+#   instance_type           = each.value["instance_type"]
+#   name                    = each.key
+#   ami_id                  = each.value["ami_id"]
+#   vpc_security_group_ids  = var.vpc_security_group_ids
+#   zone_id                 = var.zone_id
+#   env                     = var.env
+#   vault_token             = var.vault_token
+# }
 
-module "eks" {
-  source      = "./modules/eks"
-  for_each = var.eks
-  env         = var.env
-  eks_version = each.value["eks_version"]
-  subnets     = each.value["subnets"]
-  node_groups = each.value["node_groups"]
-  access =  each.value["access"]
-  addons = each.value["addons"]
-}
+# module "eks" {
+#   source      = "./modules/eks"
+#   for_each = var.eks
+#   env         = var.env
+#   eks_version = each.value["eks_version"]
+#   subnets     = each.value["subnets"]
+#   node_groups = each.value["node_groups"]
+#   access =  each.value["access"]
+#   addons = each.value["addons"]
+# }
 
 
 module "vpc" {
